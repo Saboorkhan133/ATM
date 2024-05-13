d60537a5bbda87f14968e4aed3687c8f8234296c
Saboorkhan133 <saboorkhan133@gmail.com>
2024-05-13 21:26:11 +0500

    project added

---
+++ index.js
@@ -0,0 +1,45 @@
+import inquirer from "inquirer";
+//Initialize user balance and pin code
+let myBalance = 5000;
+let myPin = 1234;
+//print welcome message
+console.log("Welcome to My - ATM Machine");
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        type: "number",
+        message: "Enter your pin code:"
+    }
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("Pin is Correct, Login Successfully!");
+    //console.log(`Current Account Balance is ${myBalance}`)
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            type: "list",
+            message: "Select an operation",
+            choices: ["Withdraw Amount", "Check Balance"]
+        }
+    ]);
+    if (operationAns.operation === "withdraw Amount") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                type: "number",
+                message: "Enter the amount to withdraw:"
+            }
+        ]);
+        if (amountAns.amount > myBalance) {
+            console.log("Insufficient Balance");
+        }
+        else {
+            myBalance -= amountAns.amount;
+            console.log(`${amountAns.amount} Withdrae Successfully`);
+            console.log(`Your Remaing Balance is: ${myBalance}`);
+        }
+    }
+    else if (operationAns.operation === "Check Balance") {
+        console.log(`Your Account Balance is: ${myBalance}`);
+    }
+}
